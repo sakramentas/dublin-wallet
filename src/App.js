3292c7bed45c7ffe2d4049c094ec038b085ee17f
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-25 17:32:13 +0100

    Added Internationalization files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import Router from '../config/routes';
-import reducers from './src/redux/reducers';
+import reducers from './redux/reducers';
+import lang from './languages/pt-br';
 
 const logger = createLogger();
 const middleware = applyMiddleware(thunk);
 const store = createStore(reducers, {}, middleware);
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Text>Test!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>{lang.SCHOOL_HERO_MESSAGE}</Text>
         </View>
       </Provider>
     );
@@ -30,7 +29,7 @@ console.disableYellowBox = true;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffb009',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/languages/pt-br.js
@@ -0,0 +1,7 @@
+const ptBr = {
+  SCHOOL_HERO_MESSAGE: 'Encontre sua próxima escola',
+  SEARCH: 'Buscar',
+  SELECT: 'Selecionar',
+};
+
+export default ptBr;
