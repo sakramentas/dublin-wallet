0114c07a44bdec5379e5aa57fd778687d36bf797
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-25 17:12:38 +0100

    Configured Redux store in the main App file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Router from '../config/routes';
+import reducers from './src/redux/reducers';
+
+const logger = createLogger();
+const middleware = applyMiddleware(thunk);
+const store = createStore(reducers, {}, middleware);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Test!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Test!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
     );
   }
 }
+
+console.disableYellowBox = true;
 
 const styles = StyleSheet.create({
   container: {
