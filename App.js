53b43765ad07d817e29fb1fa1231207fee74ba40
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-26 15:30:06 +0100

    Setup Redux Middlewares and Store

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-// import { createLogger } from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import Router from './config/routes';
-// import reducers from './src/core/redux/reducers';
+import reducers from './src/core/redux/reducers';
 import lang from './src/languages/pt-br';
 import SchoolList from './src/scenes/SchoolList';
 
-// const logger = createLogger();
-const middleware = applyMiddleware(thunk);
-// const store = createStore(reducers, {}, middleware);
+const logger = createLogger();
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(reducers, {}, middleware);
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{lang.SCHOOL_HERO_MESSAGE}</Text>
-        <SchoolList/>
-        <Text>asdadasd</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>{lang.SCHOOL_HERO_MESSAGE}</Text>
+          <SchoolList/>
+        </View>
+      </Provider>
     );
   }
 }
