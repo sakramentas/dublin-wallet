ca3170244b1d735967bff36b44740092a795e9a2
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-26 15:28:00 +0100

    Created fetchSchools actions

---
+++ src/core/redux/schools/actions.js
@@ -0,0 +1,19 @@
+import { buildFetchSchools } from './firebaseBuild';
+import {
+  FETCH_SCHOOLS,
+  FETCH_SCHOOLS_SUCCESS,
+} from './action-types';
+
+export const fetchSchools = () => (dispatch) => {
+  dispatch(fetchSchoolsAction);
+  buildFetchSchools(dispatch);
+};
+
+export const fetchSchoolsAction = {
+  type: FETCH_SCHOOLS,
+};
+
+export const fetchSchoolsSuccess = (data) => ({
+  type: FETCH_SCHOOLS_SUCCESS,
+  payload: data,
+});
