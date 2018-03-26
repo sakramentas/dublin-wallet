5352929db17f3263f6fc4709d0d630fdb3bdb840
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-26 15:28:56 +0100

    Setup Firebase environment

---
+++ src/core/firebase/firebase.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
+require("firebase/firestore");
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+// export const firebaseAuth = firebaseApp.auth();
+export const firebaseDb = firebaseApp.firestore();
+
