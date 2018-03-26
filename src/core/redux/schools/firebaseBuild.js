1f9e1ca9870caeadadb460ebf5e50e56ef90e23e
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-26 15:28:19 +0100

    Created FetchSchools query on Firebase

---
+++ src/core/redux/schools/firebaseBuild.js
@@ -0,0 +1,13 @@
+import { firebaseDb } from "../../firebase/firebase";
+import { fetchSchoolsSuccess } from "./actions";
+
+export const buildFetchSchools = dispatch => {
+  const schoolList = {};
+
+  firebaseDb.collection('schools')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach(doc => schoolList[doc.id] = doc.data());
+    })
+    .then(() => dispatch(fetchSchoolsSuccess(schoolList)));
+};
