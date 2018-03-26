5e790d1c1df6a013f113d2a94a1d30e77b7f9d49
Lucas Sacramento <lucas.arki@gmail.com>
2018-03-26 17:17:18 +0100

    Created Redux Store file

--- App.js
+++ App.js
@@ -1,38 +1,13 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { Provider } from 'react-redux';
-import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import Router from './config/routes';
-import reducers from './src/core/redux/reducers';
-import lang from './src/languages/pt-br';
-import SchoolList from './src/scenes/SchoolList';
+import store from './src/core/redux/store';
+import RootStack from './config/routes';
 
-const logger = createLogger();
-const middleware = applyMiddleware(thunk, logger);
-const store = createStore(reducers, {}, middleware);
-
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Text>{lang.SCHOOL_HERO_MESSAGE}</Text>
-          <SchoolList/>
-        </View>
-      </Provider>
-    );
-  }
-}
+const App = () =>
+  <Provider store={store}>
+    <RootStack />
+  </Provider>;
 
 console.disableYellowBox = true;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffb009',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
